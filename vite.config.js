d0b60289ef889e01f2d94581008fc98978ed909b
npcdazai <prathammandavkar932@gmail.com>
2024-12-15 16:07:14 +0530

    Dixk

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
-    port: 5173,     
+    host: "0.0.0.0",
+    port: 5173,
   },
-})
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "UNRESOLVED_IMPORT") return;
+        warn(warning);
+      },
+    },
+  },
+});
